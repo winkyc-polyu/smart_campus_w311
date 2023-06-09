c98a10116135361e1ac181984f7f2213effb2724
winkyc-polyu <ic2140@IC-L-W502G-02.hh.polyu.hk>
2023-06-09 10:31:52 +0800

    update sensor model

--- smart_campus_site/sensor/models.py
+++ smart_campus_site/sensor/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class sensor(models.Model):
+    
+    node_id = models.CharField(max_length=3)
+    node_loc = models.CharField(max_length=10)
+
+    temp = models.CharField(max_length=5)
+    hum = models.CharField(max_length=5)
+    light = models.CharField(max_length=5)
+    snd = models.CharField(max_length=5)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Sensor #{}'.format(self.id)
