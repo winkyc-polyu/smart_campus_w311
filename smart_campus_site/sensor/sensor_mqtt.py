78ce5757c9927d9e2263ded5b2c3b375afebc478
caspar <zayc666@yahoo.com.hk>
2023-06-14 11:36:53 +0800

    chart improved

--- smart_campus_site/sensor/sensor_mqtt.py
+++ smart_campus_site/sensor/sensor_mqtt.py
@@ -5,90 +5,71 @@ import logging
 from django.core.mail import send_mail
 from django.conf import settings
 from django.db.models import Avg
-
 logger = logging.getLogger('django')
 
 from django.core.mail import EmailMessage
 
+import matplotlib.pyplot as plt
+import numpy as np
+import base64
+from io import BytesIO
+from django.core.mail import EmailMessage
+# ...
+
+import io
+import matplotlib.pyplot as plt
+import paho.mqtt.client as mqtt
+from .models import Sensor
+import json
+import logging
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.db.models import Avg
+
+
+logger = logging.getLogger('django')
+
+import numpy as np
+
+from email.mime.image import MIMEImage
+
+
+logger = logging.getLogger('django')
+
+
+
+logger = logging.getLogger('django')
 
 def sendemail(value, type, des, loc, unit, avg):
     subject = 'Outlier Notification'
-    
-    # Calculate the percentage for the bar chart
-    max_value = max(value, avg)
-    value_percentage = (value / max_value) * 100
-    avg_percentage = (avg / max_value) * 100
-
-    message = f'''
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <style>
-                .red-text {{
-                    color: red;
-                }}
-                .bar-chart {{
-                    display: flex;
-                    justify-content: space-between;
-                    height: 300px;
-                    max-height: 300px;
-                }}
-                .bar-container {{
-                    width: 50%;
-                    padding: 0 2px;
-                    display: flex;
-                    justify-content: center;
-                    align-items: flex-end;
-                }}
-                .bar {{
-                    width: 50%;
-                    text-align: center;
-                }}
-                .value-bar {{
-                    background-color: red;
-                    height: {value_percentage}%;
-                }}
-                .average-bar {{
-                    background-color: #4CAF50;
-                    height: {avg_percentage}%;
-                }}
-            </style>
-        </head>
-        <body>
-            <h1><span class="red-text">! Detect unexpected Change</span></h1>
-            <p>{type} anomaly detected--{des} {value} {unit} in {loc}<br>
-            The average {type} is {avg:.2f} {unit}</p>
-            <p>Comparison:</p>
-            <div class="bar-chart">
-                <div class="bar-container">
-                    <div class="bar value-bar" style="height: {value_percentage}%;">Value: {value} {unit}</div>
-                </div>
-                <div class="bar-container">
-                    <div class="bar average-bar" style="height: {avg_percentage}%;">Average: {avg:.2f} {unit}</div>
-                </div>
-            </div>
-        </body>
-        </html>
-    '''
-
+    message = f'{type} anomaly detected -- {des} {value} {unit} in {loc}\nThe average {type} is {avg:.2f} {unit}'
     from_email = settings.DEFAULT_FROM_EMAIL
     recipient_list = [admin[1] for admin in settings.ADMINS]
-
-    email = EmailMessage(subject, message, from_email, recipient_list)
-    email.content_subtype = 'html'  # Set the email content type as HTML
-    email.send()
-
+    try:
+        fig, ax = plt.subplots()
+        ax.bar(["value", "avg"], [value, avg], color=["orange", "blue"])
+        ax.set_title(f"{type} Anomaly")
+        ax.set_ylabel(unit)
+        chart_buffer = io.BytesIO()
+        plt.savefig(chart_buffer, format='png')
+        chart_buffer.seek(0)
+        chart_filename = f"{type}_anomaly_chart.png"
+        email_msg = EmailMessage(subject, message, from_email, recipient_list)
+        email_msg.attach(chart_filename, chart_buffer.getvalue(), 'image/png')
+        email_msg.send()
+        logger.info(f"Email sent successfully to {recipient_list}")
+    except Exception as e:
+        logger.error(f"Error sending email: {str(e)}")
 
 def mqtt_on_message(client, userdata, msg):
     d_msg = str(msg.payload.decode("utf-8"))
     iotData = json.loads(d_msg)
     #if iotData["node_id"] == ID:
     print("Received message on topic %s : %s" % (msg.topic, iotData))
-    p = Sensor(node_id=iotData["node_id"], node_loc=iotData["loc"], 
-        temp=iotData["temp"], hum=iotData["hum"], light=iotData["light"], snd=iotData["snd"])
+    p = Sensor(node_id=iotData["node_id"], node_loc=iotData["loc"],temp=iotData["temp"], hum=iotData["hum"], light=iotData["light"], snd=iotData["snd"])
     p.save()
     send(p)
-
+    
 def send(p):
     inf=Sensor.objects.values('node_id','node_loc').annotate(avg_temp=Avg('temp'),avg_hum=Avg('hum'),avg_light=Avg('light'),avg_snd=Avg('snd'))
     p.temp=int(p.temp)
@@ -101,9 +82,9 @@ def send(p):
                 sendemail(p.temp,'Temperature','High Temperature',p.node_loc,"\u2103",i['avg_temp'])
             elif p.temp<i['avg_temp']-10:
                 sendemail(p.temp,'Temperature','Low Temperature',p.node_loc,"\u2103",i['avg_temp'])
-            if p.hum>i['avg_hum']+15:
+            if p.hum>i['avg_hum']+20:
                 sendemail(p.hum,'Humidity','High Humidity',p.node_loc,"%",i['avg_hum'])
-            elif p.hum<i['avg_hum']-15:
+            elif p.hum<i['avg_hum']-20:
                 sendemail(p.hum,'Humidity','Low Humidity',p.node_loc,"%",i['avg_hum'])
             if p.light>i['avg_light']+10:
                 sendemail(p.light,'Light','Intense Light',p.node_loc,"%",i['avg_light'])
@@ -112,9 +93,7 @@ def send(p):
             if p.snd>i['avg_snd']+20:
                 sendemail(p.temp,'Sound','Loud sound',p.node_loc,"dB",i['avg_snd'])
             elif p.snd<i['avg_snd']-10:
-                sendemail(p.temp,'Sound','Silent',p.node_loc,"dB",i['avg_snd'])
-        else:
-            pass
+                pass
 
 
 ID="A01" # Sensor ID
@@ -124,7 +103,7 @@ mqtt_qos = 1 # Quality of Service = 1
 mqtt_topic = "iot/sensor-A"
 
 
-mqtt_client = mqtt.Client("django-sensor-A011") # Create a Client Instance
+mqtt_client = mqtt.Client("django-sensor-A01") # Create a Client Instance
 mqtt_client.on_message = mqtt_on_message
 mqtt_client.connect(mqtt_broker, mqtt_port) # Establish a connection to a broker
 print("Connect to MQTT broker")
